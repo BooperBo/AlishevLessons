e0c094c7ddb0021260b66e253e84e3bcd328cd17
BooperBo <dennzelw@gmail.com>
2022-07-20 08:58:57 +0500

    generics

---
+++ javaCore/generics/Dog.java
@@ -0,0 +1,14 @@
+package javaCore.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Dog extends Animal {
+    @Override
+    public void eat() {
+        System.out.println("Dog is eating");
+    }
+}
+
+
+
